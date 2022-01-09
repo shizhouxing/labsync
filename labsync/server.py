2dd167b456c54132af0f6d825eacb8af76772813
Zhouxing Shi <zhouxingshichn@gmail.com>
2022-01-09 00:24:06 -0800

    Merge uploads under the same directory using one scp call only

--- labsync/server.py
+++ labsync/server.py
@@ -3,7 +3,7 @@ import time
 import threading
 import logging
 import subprocess
-from collections import deque
+from os.path import dirname
 from threading import Thread, Lock
 from subprocess import CalledProcessError
 
@@ -15,7 +15,7 @@ class Server(Thread):
         self.name = name
         self.path_root = path
         self.refresh_interval = refresh_interval
-        self.tasks = deque()
+        self.tasks = []
         self.lock = Lock()
         logger.info(f'Server {self.name}, root {self.path_root}')
 
@@ -28,18 +28,27 @@ class Server(Thread):
                 break
         if not existing:
             self.tasks.append(task)
-        logger.info('Server {}: {} task {}, {} tasks in queue'.format(
-            self.name, 'existing' if existing else 'new', task, len(self.tasks)))
+            logger.info(f'{self.name}: {task}')
         self.lock.release()
 
     def run(self):
         while True:
             while len(self.tasks) > 0:
                 self.lock.acquire()
-                task = self.tasks.pop()
+                task = self.tasks.pop(0)
+                if task['action'] == 'upload':
+                    files = [task['path']]
+                    tasks_left = []
+                    for t in self.tasks:
+                        if t['action'] == 'upload' and dirname(t['path']) == dirname(task['path']):
+                            # In the same directory. Merge it to one scp.
+                            files.append(t['path'])
+                        else:
+                            tasks_left.append(t)
+                    self.tasks = tasks_left
                 self.lock.release()
                 if task['action'] == 'upload':
-                    self._upload(task['path'])
+                    self._upload(files)
                 elif task['action'] == 'mkdir':
                     self._mkdir(task['path'])
                 elif task['action'] == 'mv':
@@ -47,29 +56,31 @@ class Server(Thread):
                 else:
                     raise ValueError('Unknown action {}'.format(task['action']))
 
-                logger.info('Server {}: {} task(s) left'.format(self.name, len(self.tasks)))
+                logger.info(f'{self.name}: {len(self.tasks)} task(s) left')
 
             time.sleep(self.refresh_interval)
 
     def _get_remote_path(self, path):
         return os.path.join(self.path_root, path)
 
-    def _upload(self, path):
-        logger.info(f'Server {self.name}: uploading {path}')
-        remote_path = self._get_remote_path(path)
-        args_scp = ['scp', path, f'{self.name}:{remote_path}']
+    def _upload(self, files):
+        logger.info(f'{self.name}: uploading {files}')
+        dn = dirname(files[0])
+        remote_path = self._get_remote_path(dn)
+        args_scp = ['scp'] + files + [f'{self.name}:{remote_path}']
         try:
             output = subprocess.check_output(args_scp, stderr=subprocess.STDOUT)
         except CalledProcessError as err:
             if 'No such file or directory' in err.output.decode():
                 # Create the missing directory and retry
-                self._mkdir(os.path.dirname(path))
+                self._mkdir(dn)
                 subprocess.run(args_scp, stderr=subprocess.STDOUT)                
 
     def _mkdir(self, path):
-        logger.info(f'Server {self.name}: mkdir {path}')
+        logger.info(f'{self.name}: mkdir {path}')
         path = self._get_remote_path(path)
-        subprocess.run(['ssh', f'{self.name}', f'mkdir {self._get_remote_path(path)}'])
+        mkdir_code = f"import os; os.makedirs('{path}',exist_ok=True)"
+        subprocess.run(['ssh', f'{self.name}', f'python3 -c "{mkdir_code}"'])
 
     def _mv(self, src_path, dest_path):
         print('TODO mv', src_path, dest_path)
